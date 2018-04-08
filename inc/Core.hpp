52e6d8aa5b8752ec2ff31414da3edeae95463637
Brice <brice.lang-nguyen@epitech.eu>
2018-04-08 02:13:05 +0200

    Add Core class
    Core class is for handle exception of all items of arcade

---
+++ inc/Core.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** cpp_arcade
+** File description:
+** Core
+*/
+#ifndef CPP_ARCADE_CORE_HPP
+#define CPP_ARCADE_CORE_HPP
+
+class Core {
+};
+
+#endif //CPP_ARCADE_CORE_HPP
